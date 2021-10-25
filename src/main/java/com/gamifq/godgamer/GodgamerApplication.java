776c91f0815bc8d544241d5e3f8b4ab7c5076595
silas-sousa <silas.sousa@ufu.br>
2021-10-25 20:11:04 -0300

    first commit

---
+++ src/main/java/com/gamifq/godgamer/GodgamerApplication.java
@@ -0,0 +1,13 @@
+package com.gamifq.godgamer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GodgamerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GodgamerApplication.class, args);
+	}
+
+}
